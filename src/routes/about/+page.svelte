dd3fefdf91348d00232f4b4ff9a3bf5ad639f9c9
et-web-dev <130565352+et-web-dev@users.noreply.github.com>
2023-09-10 19:16:51 -0400

    Init App

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -23,6 +23,4 @@
 		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
 		using it with JavaScript disabled!
 	</p>
-	<br>
-	<p>Enjoy it!</p>
 </div>
